ee424d38bff902949f339f8dff1ebaf721e90a25
Joel Lanciaux <joel.lanciaux@gmail.com>
2015-10-24 17:11:44 -0400

    Initial commit.

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+import SpacerRow from './spacer-row';
+import TableBody from './table-body';
+import Table from './table';
+
+export { SpacerRow as SpacerRow };
+export { TableBody as TableBody };
+export { Table as Table };
