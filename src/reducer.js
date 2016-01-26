b53b49a90275b658a7b5703aee7859d1e30bc74c
Joel Lanciaux <joel.lanciaux@gmail.com>
2016-01-26 01:38:20 -0500

    Removed commented out debug code.

--- src/reducer.js
+++ src/reducer.js
@@ -33,26 +33,3 @@ export function GRIDDLE_SORT_AFTER(state, action, helpers) {
 export function GRIDDLE_TOGGLE_COLUMN_AFTER(state, action, helpers) {
   return helpers.updateRenderedData(state, helpers);
 }
-
-
-// export function XY_POSITION_CHANGED(state, action, helpers) {
-//   return helpers.updatePositionProperties(action, state, helpers);
-// }
-
-// export function GRIDDLE_LOADED_DATA_AFTER(state, action, helpers) {
-//   return helpers.updatePositionProperties({ yScrollPosition: 0, xScrollPosition: 0, force: true}, state, helpers, true);
-// }
-
-// export function GRIDDLE_FILTERED_AFTER(state, action, helpers) {
-//   return helpers.setCurrentPosition(state, 0, 0);
-// }
-
-// const actions = ['XY_POSITION_CHANGED', 'GRIDDLE_LOADED_DATA', 'GRIDDLE_GET_PAGE', 'GRIDDLE_NEXT_PAGE',
-//                  'GRIDDLE_PREVIOUS_PAGE', 'GRIDDLE_FILTERED', 'GRIDDLE_SORT', 'GRIDDLE_TOGGLE_COLUMN'];
-
-// export function AFTER_REDUCE(state, action, helpers) {
-//   if (actions.includes(action.type)) {
-//     return helpers.updateRenderedData(state, helpers);
-//   }
-//   return state;
-// }
