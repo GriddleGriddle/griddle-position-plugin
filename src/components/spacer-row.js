85ba956ac6e5e465b1846f76161f6d74a9241b36
Ryan Lanciaux <rlanciaux@opengov.com>
2016-06-08 13:50:26 -0400

    0.0.30

--- src/components/spacer-row.js
+++ src/components/spacer-row.js
@@ -18,9 +18,16 @@ class SpacerRow extends React.Component {
     super(props);
   }
 
+  //This gets one column property object from the global property object
+  getColumnPropertyObject(columnProperties, columnName) {
+    return columnProperties.hasOwnProperty(columnName) ?
+      columnProperties[columnName] :
+      null;
+  }
+
   render() {
     let height = 0, spacerRowStyle = {};
-    const {placement, currentPosition, positionConfig, renderProperties} = this.props;
+    const { placement, currentPosition, positionConfig, renderProperties, renderedData } = this.props;
 
     if (currentPosition) {
       // Get the length of rows that the spacer row will represent.
@@ -34,8 +41,11 @@ class SpacerRow extends React.Component {
     spacerRowStyle.height = height + 'px';
     return (
       <tr key={placement + '-' + height} style={spacerRowStyle}>
-        {Object.keys(renderProperties.columnProperties).map(columnName => {
-          return <td key={columnName} style={{width: renderProperties.columnProperties[columnName].width || 'auto'}}></td>
+        {Object.keys(renderedData.length > 0 ? renderedData[0] : renderedData.columnProperties)
+            .filter(k => renderProperties.metadataColumn !== k)
+            .map(columnName => {
+          const columnProperty = this.getColumnPropertyObject(renderProperties.columnProperties, columnName);
+          return <td key={columnName} {...columnProperty} className={columnProperty.cssClassName}></td>
         })}
       </tr>
     );
